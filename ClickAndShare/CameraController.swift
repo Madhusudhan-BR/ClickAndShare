5bbc4cede1610bbe167804da79426b5edf9e5ef1
Madhusudhan-BR <madhusudhanbram@gmail.com>
2017-07-20 16:50:16 +0530

    completed camera controller

--- ClickAndShare/CameraController.swift
+++ ClickAndShare/CameraController.swift
@@ -10,9 +10,41 @@ import UIKit
 import AVFoundation
 
 class CameraController: UIViewController {
+    
+    
+    let captureButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "capture_photo").withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handleCaptureButton), for: .touchUpInside)
+        return button
+    }()
+    
+    func handleCaptureButton(){
+        print("handling capture button ")
+    }
+    
+    let dismissButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "right_arrow_shadow").withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handleDismissButton), for: .touchUpInside)
+        return button
+    }()
+    
+    func handleDismissButton(){
+        dismiss(animated: true, completion: nil)
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupCameraSession()
+        
+        view.addSubview(captureButton)
+        captureButton.anchor(top: nil, left: nil, bottom: view.bottomAnchor, right: nil, paddingTop: 0, paddingLeft: 0, paddingBottom: -24, paddingRight: 0, width: 80, height: 80)
+        captureButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        view.addSubview(dismissButton)
+        dismissButton.anchor(top: view.topAnchor, left: nil, bottom: nil, right: view.rightAnchor, paddingTop: 12, paddingLeft: 0, paddingBottom: 0, paddingRight: 12, width: 60, height: 60)
     }
     
     fileprivate func setupCameraSession(){
