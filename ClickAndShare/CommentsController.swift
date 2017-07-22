7ab03c0dc1636496cc68064526814c49a5f2c437
Madhusudhan-BR <madhusudhanbram@gmail.com>
2017-07-22 13:59:15 +0530

    implemented a custom input contanier view

--- ClickAndShare/CommentsController.swift
+++ ClickAndShare/CommentsController.swift
@@ -9,8 +9,50 @@
 import UIKit
 
 class CommentsController: UICollectionViewController  {
+   
+    
+    let containerView: UIView = {
+        let container = UIView()
+        container.backgroundColor = UIColor.green
+        return container
+    }()
+    
+    let inpuTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Enter a comment"
+        return tf
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView?.backgroundColor = UIColor.white 
+        navigationItem.title = "Comments"
+        collectionView?.backgroundColor = UIColor.white
+        containerView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 100)
+        containerView.addSubview(inpuTextField)
+        inpuTextField.anchor(top: containerView.topAnchor, left: containerView.leftAnchor, bottom: containerView.bottomAnchor, right: containerView.rightAnchor, paddingTop: 0, paddingLeft: 12, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
     }
+    
+    override var inputAccessoryView: UIView? {
+        get {
+            return containerView
+        }
+    }
+    
+    override var canBecomeFirstResponder: Bool {
+        return true
+    }
+    
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        tabBarController?.tabBar.isHidden = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tabBarController?.tabBar.isHidden = true
+    }
+    
+    
+    
 }
