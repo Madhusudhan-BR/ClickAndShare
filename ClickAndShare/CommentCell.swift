6cdac1663a0a9d8804cec00ea312ed162243a29e
Madhusudhan-BR <madhusudhanbram@gmail.com>
2017-07-23 00:29:08 +0530

    minor fix

--- ClickAndShare/CommentCell.swift
+++ ClickAndShare/CommentCell.swift
@@ -15,7 +15,7 @@ class CommentCell: UICollectionViewCell {
             self.profileImageView.loadImage(urlString: comment?.user?.profileImageURL ?? "")
             let attributedText = NSMutableAttributedString(string: comment?.user?.username ?? "" , attributes: [NSFontAttributeName: UIFont.boldSystemFont(ofSize: 14)])
             attributedText.append(NSAttributedString(string:  " " + (comment?.text)! , attributes: [NSFontAttributeName: UIFont.systemFont(ofSize: 14)]))
-            self.commentLabel.attributedText = attributedText
+            self.commentView.attributedText = attributedText
         }
     }
 
@@ -37,25 +37,27 @@ class CommentCell: UICollectionViewCell {
         return label
     }()
     
-    var commentLabel: UILabel = {
-        let label = UILabel()
-        label.font = UIFont.systemFont(ofSize: 14)
-        label.numberOfLines = 0
-        label.text = "This is a comment. i am testing the comment. Do you realise this may go to the next line now?"
-        return label
+    var commentView: UITextView = {
+        let tv = UITextView()
+        tv.isScrollEnabled = false
+        tv.isEditable = false
+        tv.font = UIFont.systemFont(ofSize: 14)
+        //tv.numberOfLines = 0
+        //tv.text = "This is a comment. i am testing the comment. Do you realise this may go to the next line now?"
+        return tv
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubview(profileImageView)
-        addSubview(commentLabel)
+        addSubview(commentView)
         profileImageView.anchor(top: topAnchor, left: leftAnchor, bottom: nil, right: nil, paddingTop: 8, paddingLeft: 8, paddingBottom: 0, paddingRight: 0, width: 40, height: 40)
-        commentLabel.anchor(top: topAnchor ,left: profileImageView.rightAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 8, paddingLeft: 8, paddingBottom: -8, paddingRight: 8, width: 0, height: 0)
+        commentView.anchor(top: topAnchor ,left: profileImageView.rightAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 8, paddingLeft: 8, paddingBottom: -8, paddingRight: 8, width: 0, height: 0)
         
         let seperatorView = UIView()
         seperatorView.backgroundColor = UIColor(white: 0, alpha: 0.3)
         addSubview(seperatorView)
-        seperatorView.anchor(top: bottomAnchor, left: commentLabel.leftAnchor, bottom: nil, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0.5)
+        seperatorView.anchor(top: bottomAnchor, left: commentView.leftAnchor, bottom: nil, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0.5)
         
         
        
